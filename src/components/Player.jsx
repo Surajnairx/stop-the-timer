0c800ab0e255421cb3acdc726271cc2ab03ec917
Suraj Nair <surajnair9698@gmail.com>
2025-10-01 18:28:55 +0530

    feat: implemented the Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,17 @@
+import { useState, useRef } from "react";
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const getPlayerInput = useRef();
+  function handleClick() {
+    setPlayerName(getPlayerInput.current.value);
+    getPlayerInput.current.value = "";
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ? playerName : "unknown entity"} </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={getPlayerInput} type="text" />
+        <button onClick={handleClick}>Enter Name</button>
       </p>
     </section>
   );
