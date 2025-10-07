db44c7f20adf6a411132e5d360f8553cc33847d5
Suraj Nair <surajnair9698@gmail.com>
2025-10-07 18:39:06 +0530

    feat : workout timer logic and UI handling

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,34 @@
+import { useState, useRef } from "react";
+
 const TimerChallenge = ({ title, targetTime }) => {
+  const timer = useRef();
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpeired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpeired && <p>You lost!</p>}
       <p className="challenge-time">
-        {targetTime} second {targetTime > 1 ? "s" : ""}
+        {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Button</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">Time is running... / Timer inactive</p>
+      <p className={timerStarted ? "active" : null}>
+        {timerStarted ? "Time is running" : "Timer inactive"}
+      </p>
     </section>
   );
 };
